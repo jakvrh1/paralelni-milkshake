147472e98145c046dd8170fe22ed2ca8f1fa77a3
Adrijan Rogan <adrijanrogan@protonmail.com>
2021-01-01 23:34:04 +0100

    Run 100 times

--- sequential-c/sequential.cpp
+++ sequential-c/sequential.cpp
@@ -12,36 +12,14 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  auto image_data = Input::read("../assets/1.png");
-  auto rle_data = RLE::encode(image_data);
-  auto huffman_data = Huffman::encode(rle_data);
-  
-  Output::write("test.txt", huffman_data); 
-  
-  /*
-  for(int i = 0; i < huffman_data.size(); ++i) {
-    for(int j = 0; j < rle_data[i].size(); ++j) {
-      cout << "(" << rle_data[i][j].first << "," << rle_data[i][j].second << ") ";
-    }
-    cout << endl;
-    for(int j = 0; j < huffman_data[i].size(); ++j) {
-      cout << huffman_data[i][j] << " ";
-    }
-    cout << endl;
-    if(i == 10) break;
-  }*/
 
-   
-   
-
-
-  /*
-  for (auto &i : rle_data) {
-    for (auto &j : i) std::cout << j.first << " " << j.second << ", ";
-    std::cout << "\n";
+  for (int i = 0; i < 100; i++) {
+    auto image_data = Input::read("../assets/1.png");
+    auto rle_data = RLE::encode(image_data);
+    auto huffman_data = Huffman::encode(rle_data);
+    
+    Output::write("test.txt", huffman_data); 
   }
-  std::cout << std::flush;
-  */
 
   return 0;
 }
