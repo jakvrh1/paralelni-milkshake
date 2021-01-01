10f4156e047c2cbfb6fcf8b8fcca0d32d96a6127
Adrijan Rogan <adrijanrogan@protonmail.com>
2021-01-01 22:55:15 +0100

    Implement output

--- sequential-c/io/output.hpp
+++ sequential-c/io/output.hpp
@@ -6,4 +6,41 @@
 #ifndef OUTPUT_HPP
 #define OUTPUT_HPP
 
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Output {
+
+  public:
+
+    /**
+     * Zapiše vsebino [data] po vrsticah.
+     */
+    static void write(std::string filename, std::vector<std::vector<std::string>> data) {
+      std::ofstream file;
+      file.open(filename);
+
+      if (!file.good()) {
+        throw std::invalid_argument("Cannot open file");
+      }
+
+      // Izpišemo vsako vrstico
+      for (auto &line : data) {
+        // Znotraj vrstice izpišemo besede s presledki
+        for (auto &text : line) {
+          file << text << " ";
+        }
+        file << std::endl;
+      }
+
+      if (!file.good()) {
+        throw std::invalid_argument("Writing to file failed");
+      }
+
+      file.close();
+    }
+
+};
+
 #endif
