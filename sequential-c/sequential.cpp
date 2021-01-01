54131d688ec00ab00c8695fb9dd954315b03c7cb
Jaka Vrhovec <jakvrh1@gmail.com>
2021-01-01 21:42:25 +0100

    added sample in main for Input::read and RLE::encode

--- sequential-c/sequential.cpp
+++ sequential-c/sequential.cpp
@@ -3,13 +3,23 @@
  */
 
 #include <iostream>
+#include <random>
 
 #include "encoder/encoder.hpp"
 #include "io/input.hpp"
 #include "io/output.hpp"
 
+using namespace std;
+
 int main(int argc, char const *argv[]) {
-  std::cout << "Hello world" << std::endl;
+  auto image_data = Input::read("../assets/1.png");
+  auto rle_data = RLE::encode(image_data);
+
+  for (auto &i : rle_data) {
+    for (auto &j : i) std::cout << j.first << " " << j.second << ", ";
+    std::cout << "\n";
+  }
+  std::cout << std::flush;
 
   return 0;
 }
