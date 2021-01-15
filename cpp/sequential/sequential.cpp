8729e0e3d84bd00e65773c1729442bdd5593c542
Jaka Vrhovec <jakvrh1@gmail.com>
2021-01-15 21:50:42 +0100

    encoder hpp splitted into multiple files

--- cpp/sequential/sequential.cpp
+++ cpp/sequential/sequential.cpp
@@ -5,23 +5,25 @@
 #include <iostream>
 #include <random>
 
-#include "../encoder.hpp"
+#include "../rle.hpp"
+#include "../huffman.hpp"
+#include "../tester.hpp"
 #include "../input.hpp"
 #include "../output.hpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  /*
+  
   for (int i = 0; i < 100; i++) {
-    auto image_data = Input::read("../../assets/1.png");
+    auto image_data = Input::read("../../assets/2.png");
     auto rle_data = RLE::encode(*image_data);
 
     Huffman *hf = Huffman::initialize(rle_data);
     //hf->black_tree->disp();
     //hf->white_tree->disp();
 
-    Output::write("test.txt", hf->encode());
+    //Output::write("test.txt", hf->encode());
 
     hf->finalize();
 
@@ -30,13 +32,15 @@ int main(int argc, char const *argv[]) {
     //Output::write("test.txt", huffman_data); 
     break;
   }
-  */
+  
 
-  std::srand(123);
+  /*
+  std::srand(523);
   for(int i = 0; i < 1; ++i) {
-    RLE::testRLE(10, true);
+    RLE::testRLE(1000, true);
   }
 
+  */
   /*
   while(true) {
     RLE::testRLE(10, true);
