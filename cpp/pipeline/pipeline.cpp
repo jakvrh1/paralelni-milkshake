e18876cb29197c46508f08ec84210bc269bbaf95
Adrijan Rogan <adrijanrogan@protonmail.com>
2021-01-17 12:11:17 +0100

    Pass around pointers and properly free memory

--- cpp/pipeline/pipeline.cpp
+++ cpp/pipeline/pipeline.cpp
@@ -15,6 +15,11 @@
 
 #define REPS 250
 
+struct huffman_data {
+  Huffman *hf;
+  Vec<string> *encoded;
+};
+
 using namespace std;
 
 // Funkcija za niti, ki berejo.
@@ -35,21 +40,23 @@ void* read(void* arg) {
 
 // Funkcija za niti, ki prebrane podatke kodirajo z RLE. 
 void* rle(void* arg) {
-  PipelineStage<int, Vec<bool>*, Vec<int_bool>>* stage = (PipelineStage<int, Vec<bool>*, Vec<int_bool>>*) arg;
+  PipelineStage<int, Vec<bool>*, Vec<int_bool>*>* stage = (PipelineStage<int, Vec<bool>*, Vec<int_bool>*>*) arg;
 
   while (true) {
     auto p = stage->consume(); 
     auto key = p.first;
     auto image_data = p.second;
 
-    auto rle_data = RLE::encode(*image_data);
+    auto rle_data = RLE::encode(image_data);
+    delete image_data;
+
     stage->produce(key, rle_data);
   }
 }
 
 // Funkcija za niti, ki RLE podatke kodirajo s huffmanom.  
 void* huffman(void* arg) {
-  PipelineStage<int, Vec<int_bool>, Vec<string>>* stage = (PipelineStage<int, Vec<int_bool>, Vec<string>>*) arg;
+  PipelineStage<int, Vec<int_bool>*, huffman_data> *stage = (PipelineStage<int, Vec<int_bool>*, huffman_data>*) arg;
 
   while (true) {
     auto p = stage->consume(); 
@@ -57,21 +64,30 @@ void* huffman(void* arg) {
     auto rle_data = p.second;
 
     Huffman *hf = Huffman::initialize(rle_data);
-    stage->produce(key, hf->encode());
-    hf->finalize();
+    auto encoded = hf->encode();
+    delete rle_data;
+
+    huffman_data data;
+    data.hf = hf;
+    data.encoded = encoded;
+
+    stage->produce(key, data);
   }
 }
 
 // Funkcija za niti, ki pisejo.
 void* write(void* arg) {
-  PipelineStage<int, Vec<string>, void>* stage = (PipelineStage<int, Vec<string>, void>*) arg;
+  PipelineStage<int, huffman_data, void>* stage = (PipelineStage<int, huffman_data, void>*) arg;
 
   for (int i = 0; i < REPS; i++) {
     auto p = stage->consume(); 
     auto key = p.first;
-    auto huffman_data = p.second;
-
-    Output::write("test.txt", huffman_data);
+    auto data = p.second;
+    auto header = data.hf->header();
+    
+    Output::write("test.txt", header, data.encoded);
+    data.hf->finalize();
+    delete data.encoded;
   }
 
   // S tem se bo program zaključil (glavna nit čaka na join)
@@ -83,20 +99,20 @@ int main(int argc, char const *argv[]) {
 
   FifoStream<int, string> input_stream;
   FifoStream<int, Vec<bool>*> bit_stream;
-  FifoStream<int, Vec<int_bool>> encoded_stream;
-  FifoStream<int, Vec<string>> output_stream;
+  FifoStream<int, Vec<int_bool>*> encoded_stream;
+  FifoStream<int, huffman_data> output_stream;
 
   // Prva stopnja cevovoda, image reading
   PipelineStage<int, string, Vec<bool>*> read_stage(&input_stream, &bit_stream);  
 
   // Druga stopnja cevovoda, run length encoding
-  PipelineStage<int, Vec<bool>*, Vec<int_bool>> rle_stage(&bit_stream, &encoded_stream);
+  PipelineStage<int, Vec<bool>*, Vec<int_bool>*> rle_stage(&bit_stream, &encoded_stream);
 
   // Tretja stopnja cevovoda, huffman encoding
-  PipelineStage<int, Vec<int_bool>, Vec<string>> huffman_stage(&encoded_stream, &output_stream);
+  PipelineStage<int, Vec<int_bool>*, huffman_data> huffman_stage(&encoded_stream, &output_stream);
 
   // Cetrta stopnja cevovoda, writing
-  PipelineStage<int, Vec<string>, void> write_stage(&output_stream);
+  PipelineStage<int, huffman_data, void> write_stage(&output_stream);
 
   pthread_t read_thread;
   pthread_t rle_thread;
