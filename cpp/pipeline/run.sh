629c2555b59253e844f72ce7071e22512509298f
Adrijan Rogan <adrijanrogan@protonmail.com>
2021-01-19 23:47:27 +0100

    Update gitignore

--- cpp/pipeline/run.sh
+++ cpp/pipeline/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Compiling"
-g++ -std=c++11 -O2 -lpthread pipeline.cpp -o pipeline 
+g++ -std=c++11 -O2 -lpthread pipeline.cpp -o pipeline.o 
 
 echo "Starting time measurement"
-time for i in {1..10} ; do ./pipeline ; done
+time for i in {1..10} ; do ./pipeline.o ; done
 
 echo "Starting memory measurement"
-for i in {1..10}; do /usr/bin/time -f "%M" ./pipeline; done
+for i in {1..10}; do /usr/bin/time -f "%M" ./pipeline.o; done
 
-#perf record ./pipeline 
+#perf record ./pipeline.o
--- cpp/pipesquare/run.sh
+++ cpp/pipesquare/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Compiling"
-g++ -std=c++11 -O2 -lpthread pipesquare.cpp -o pipesquare 
+g++ -std=c++11 -O2 -lpthread pipesquare.cpp -o pipesquare.o
 
 echo "Starting time measurement"
-time for i in {1..10} ; do ./pipesquare ; done
+time for i in {1..10} ; do ./pipesquare.o ; done
 
 echo "Starting memory measurement"
-for i in {1..10}; do /usr/bin/time -f "%M" ./pipesquare; done
+for i in {1..10}; do /usr/bin/time -f "%M" ./pipesquare.o; done
 
-#perf record ./pipesquare 
+#perf record ./pipesquare.o 
